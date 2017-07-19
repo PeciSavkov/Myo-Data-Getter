6972d9be047975a86b7fe81060910d85c9ce4f55
PeciSavkov <ptrsavkov@gmail.com>
2017-07-19 12:19:05 +0300

    Program

---
+++ MyoTest/MyoTest/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace MyoTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rtbRPY = new System.Windows.Forms.RichTextBox();
+            this.rtbGestures = new System.Windows.Forms.RichTextBox();
+            this.chSensor1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chSensor2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chSensor3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chSensor4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chSensor8 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chSensor7 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chSensor6 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chSensor5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.запазиФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.изходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor5)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbRPY
+            // 
+            this.rtbRPY.Location = new System.Drawing.Point(12, 27);
+            this.rtbRPY.Name = "rtbRPY";
+            this.rtbRPY.Size = new System.Drawing.Size(482, 179);
+            this.rtbRPY.TabIndex = 0;
+            this.rtbRPY.Text = "";
+            // 
+            // rtbGestures
+            // 
+            this.rtbGestures.Location = new System.Drawing.Point(500, 26);
+            this.rtbGestures.Name = "rtbGestures";
+            this.rtbGestures.Size = new System.Drawing.Size(482, 180);
+            this.rtbGestures.TabIndex = 1;
+            this.rtbGestures.Text = "";
+            // 
+            // chSensor1
+            // 
+            chartArea1.Name = "s1Value";
+            this.chSensor1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chSensor1.Legends.Add(legend1);
+            this.chSensor1.Location = new System.Drawing.Point(12, 212);
+            this.chSensor1.Name = "chSensor1";
+            series1.ChartArea = "s1Value";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Сензор 1";
+            this.chSensor1.Series.Add(series1);
+            this.chSensor1.Size = new System.Drawing.Size(482, 177);
+            this.chSensor1.TabIndex = 2;
+            this.chSensor1.Text = "chart1";
+            // 
+            // chSensor2
+            // 
+            chartArea2.Name = "s1Value";
+            this.chSensor2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chSensor2.Legends.Add(legend2);
+            this.chSensor2.Location = new System.Drawing.Point(500, 212);
+            this.chSensor2.Name = "chSensor2";
+            series2.ChartArea = "s1Value";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Сензор 2";
+            this.chSensor2.Series.Add(series2);
+            this.chSensor2.Size = new System.Drawing.Size(482, 177);
+            this.chSensor2.TabIndex = 3;
+            this.chSensor2.Text = "chart1";
+            // 
+            // chSensor3
+            // 
+            chartArea3.Name = "s1Value";
+            this.chSensor3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chSensor3.Legends.Add(legend3);
+            this.chSensor3.Location = new System.Drawing.Point(11, 395);
+            this.chSensor3.Name = "chSensor3";
+            series3.ChartArea = "s1Value";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Сензор 3";
+            this.chSensor3.Series.Add(series3);
+            this.chSensor3.Size = new System.Drawing.Size(482, 177);
+            this.chSensor3.TabIndex = 4;
+            this.chSensor3.Text = "chart2";
+            // 
+            // chSensor4
+            // 
+            chartArea4.Name = "s1Value";
+            this.chSensor4.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chSensor4.Legends.Add(legend4);
+            this.chSensor4.Location = new System.Drawing.Point(500, 395);
+            this.chSensor4.Name = "chSensor4";
+            series4.ChartArea = "s1Value";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Сензор 4";
+            this.chSensor4.Series.Add(series4);
+            this.chSensor4.Size = new System.Drawing.Size(482, 177);
+            this.chSensor4.TabIndex = 5;
+            this.chSensor4.Text = "chart3";
+            // 
+            // chSensor8
+            // 
+            chartArea5.Name = "s1Value";
+            this.chSensor8.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chSensor8.Legends.Add(legend5);
+            this.chSensor8.Location = new System.Drawing.Point(499, 761);
+            this.chSensor8.Name = "chSensor8";
+            series5.ChartArea = "s1Value";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Сензор 8";
+            this.chSensor8.Series.Add(series5);
+            this.chSensor8.Size = new System.Drawing.Size(482, 177);
+            this.chSensor8.TabIndex = 9;
+            this.chSensor8.Text = "chart4";
+            // 
+            // chSensor7
+            // 
+            chartArea6.Name = "s1Value";
+            this.chSensor7.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chSensor7.Legends.Add(legend6);
+            this.chSensor7.Location = new System.Drawing.Point(10, 761);
+            this.chSensor7.Name = "chSensor7";
+            series6.ChartArea = "s1Value";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Сензор 7";
+            this.chSensor7.Series.Add(series6);
+            this.chSensor7.Size = new System.Drawing.Size(482, 177);
+            this.chSensor7.TabIndex = 8;
+            this.chSensor7.Text = "chart5";
+            // 
+            // chSensor6
+            // 
+            chartArea7.Name = "s1Value";
+            this.chSensor6.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chSensor6.Legends.Add(legend7);
+            this.chSensor6.Location = new System.Drawing.Point(499, 578);
+            this.chSensor6.Name = "chSensor6";
+            series7.ChartArea = "s1Value";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Legend = "Legend1";
+            series7.Name = "Сензор 6";
+            this.chSensor6.Series.Add(series7);
+            this.chSensor6.Size = new System.Drawing.Size(482, 177);
+            this.chSensor6.TabIndex = 7;
+            this.chSensor6.Text = "chart6";
+            // 
+            // chSensor5
+            // 
+            chartArea8.Name = "s1Value";
+            this.chSensor5.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chSensor5.Legends.Add(legend8);
+            this.chSensor5.Location = new System.Drawing.Point(11, 578);
+            this.chSensor5.Name = "chSensor5";
+            series8.ChartArea = "s1Value";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Legend = "Legend1";
+            series8.Name = "Сензор 5";
+            this.chSensor5.Series.Add(series8);
+            this.chSensor5.Size = new System.Drawing.Size(482, 177);
+            this.chSensor5.TabIndex = 6;
+            this.chSensor5.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1000, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.запазиФайлToolStripMenuItem,
+            this.изходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // запазиФайлToolStripMenuItem
+            // 
+            this.запазиФайлToolStripMenuItem.Name = "запазиФайлToolStripMenuItem";
+            this.запазиФайлToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.запазиФайлToolStripMenuItem.Text = "Запази файл";
+            this.запазиФайлToolStripMenuItem.Click += new System.EventHandler(this.запазиФайлToolStripMenuItem_Click);
+            // 
+            // изходToolStripMenuItem
+            // 
+            this.изходToolStripMenuItem.Name = "изходToolStripMenuItem";
+            this.изходToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.изходToolStripMenuItem.Text = "Изход";
+            this.изходToolStripMenuItem.Click += new System.EventHandler(this.изходToolStripMenuItem_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text Document (*.txt)|*.txt|All files (*.*)|*.*\"";
+            this.saveFileDialog1.RestoreDirectory = true;
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 951);
+            this.Controls.Add(this.chSensor8);
+            this.Controls.Add(this.chSensor7);
+            this.Controls.Add(this.chSensor6);
+            this.Controls.Add(this.chSensor5);
+            this.Controls.Add(this.chSensor4);
+            this.Controls.Add(this.chSensor3);
+            this.Controls.Add(this.chSensor2);
+            this.Controls.Add(this.chSensor1);
+            this.Controls.Add(this.rtbGestures);
+            this.Controls.Add(this.rtbRPY);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Myo Data Getter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chSensor5)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbRPY;
+        private System.Windows.Forms.RichTextBox rtbGestures;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chSensor1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chSensor2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chSensor3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chSensor4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chSensor8;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chSensor7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chSensor6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chSensor5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem запазиФайлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem изходToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ MyoTest/MyoTest/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Windows.Forms;
+
+using MyoSharp.Device;
+using MyoSharp.Communication;
+using MyoSharp.Exceptions;
+using MyoSharp.Poses;
+using System.IO;
+
+namespace MyoTest
+{
+    public partial class Form1 : Form
+    {
+        IChannel myoChannel;
+        IHub myoHub;
+        string text;
+        public int[] sen = new int[8];
+        string rpy;
+        string emg;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeMyo();
+            timer1.Start();
+        }
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+            StopMyo();
+            Application.Exit();
+        }
+
+        private void InitializeMyo()
+        {
+            myoChannel = Channel.Create(ChannelDriver.Create(ChannelBridge.Create(), MyoErrorHandlerDriver.Create(MyoErrorHandlerBridge.Create())));
+            myoHub = Hub.Create(myoChannel);
+
+            myoHub.MyoConnected += MyoHub_MyoConnected;
+            myoHub.MyoDisconnected += MyoHub_MyoDisconnected;
+
+            myoChannel.StartListening();
+        }
+
+        private void StopMyo()
+        {
+            myoChannel.StopListening();
+            myoChannel.Dispose();
+        }
+
+        #region Event_Handlers
+        private void MyoHub_MyoConnected(object sender, MyoEventArgs e)
+        {
+            MessageBox.Show("Myo е свързано!");
+            
+            e.Myo.Vibrate(VibrationType.Long);
+
+            e.Myo.Unlock(UnlockType.Hold);
+
+            var pose = HeldPose.Create(e.Myo, Pose.DoubleTap, Pose.FingersSpread, Pose.Fist, Pose.WaveIn, Pose.WaveOut);
+            pose.Interval = TimeSpan.FromSeconds(2);
+            pose.Start();
+            
+            pose.Triggered += Pose_Triggered;
+            e.Myo.OrientationDataAcquired += Myo_OrientationDataAcquired;
+            e.Myo.EmgDataAcquired += Myo_EmgDataAcquired;
+            e.Myo.SetEmgStreaming(true);
+        }
+
+        private void Myo_EmgDataAcquired(object sender, EmgDataEventArgs e)
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                sen[i] = e.EmgData.GetDataForSensor(i);
+            }
+            emg = "Sensor1: " + Convert.ToString( e.Myo.EmgData.GetDataForSensor(0)) + " Sensor2: " + Convert.ToString(e.Myo.EmgData.GetDataForSensor(1)) + " Sensor3: " + Convert.ToString(e.Myo.EmgData.GetDataForSensor(2))
+                + " Sensor4: " + Environment.NewLine + Convert.ToString(e.Myo.EmgData.GetDataForSensor(3)) + " Sensor5: " + Convert.ToString(e.Myo.EmgData.GetDataForSensor(4) + " Sensor6: " + Convert.ToString(e.Myo.EmgData.GetDataForSensor(5))
+                 + " Sensor7: " + Convert.ToString(e.Myo.EmgData.GetDataForSensor(6)) + " Sensor8: " + Convert.ToString(e.Myo.EmgData.GetDataForSensor(7)));
+        }
+
+        private void Myo_OrientationDataAcquired(object sender, OrientationDataEventArgs e)
+        {
+            const float PI = (float)System.Math.PI;
+            var roll = (e.Roll + PI) / (PI * 2.0f) * 100;
+            var pitch = (e.Pitch + PI) / (PI * 2.0f) * 100;
+            var yaw = (e.Yaw + PI) / (PI * 2.0f) * 100;
+            rpy = Convert.ToString(roll) + " " + Convert.ToString(pitch) + " " + Convert.ToString(yaw);
+            string data = "Roll: " + roll.ToString("#.##") + Environment.NewLine +
+                           "Pitch: " + pitch.ToString("#.##") + Environment.NewLine +
+                           "Yaw: " + yaw.ToString("#.##");
+            InvokeDataRPY(data);
+        }
+
+        private void Pose_Triggered(object sender, PoseEventArgs e)
+        {
+            text += DateTime.Now.ToString("HH:mm:ss.fff", System.Globalization.DateTimeFormatInfo.InvariantInfo) + " " + e.Pose.ToString() + Environment.NewLine + rpy + Environment.NewLine + emg + Environment.NewLine;
+            InvokeData(e.Pose.ToString());
+        }
+
+        private void MyoHub_MyoDisconnected(object sender, MyoEventArgs e)
+        {
+            MessageBox.Show("Myo е изключено!");
+        }
+        #endregion
+        void InvokeData(string Data)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(InvokeData), new object[] { Data });
+                return;
+            }
+            rtbGestures.AppendText(Data + Environment.NewLine);
+        }
+        void InvokeDataRPY(string Data)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(InvokeDataRPY), new object[] { Data });
+                return;
+            }
+            rtbRPY.Clear();
+            rtbRPY.AppendText(Data + Environment.NewLine);
+        }
+
+        private void RefreshGraph()
+        {
+            chSensor1.Series["Сензор 1"].Points.AddY(sen[0]);
+            chSensor2.Series["Сензор 2"].Points.AddY(sen[1]);
+            chSensor3.Series["Сензор 3"].Points.AddY(sen[2]);
+            chSensor4.Series["Сензор 4"].Points.AddY(sen[3]);
+            chSensor5.Series["Сензор 5"].Points.AddY(sen[4]);
+            chSensor6.Series["Сензор 6"].Points.AddY(sen[5]);
+            chSensor7.Series["Сензор 7"].Points.AddY(sen[6]);
+            chSensor8.Series["Сензор 8"].Points.AddY(sen[7]);
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            RefreshGraph();
+        }
+
+        private void запазиФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void изходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //saveFileDialog1.FileName = DateTime.Now.ToString();
+            string Name = saveFileDialog1.FileName;
+            File.WriteAllText(Name, text);
+        }
+    }
+}
